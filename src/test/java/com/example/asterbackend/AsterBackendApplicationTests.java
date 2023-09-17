ebd46b6d9665c4443c290fd645abe711ff707105
user <b57062562@gmail.com>
2023-09-18 08:17:46 +0900

    Initial commit

---
+++ src/test/java/com/example/asterbackend/AsterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
