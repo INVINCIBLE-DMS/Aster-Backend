c9a18b8127cdb5b0d7c7a58f1d22c77b2172ba30
user <b57062562@gmail.com>
2023-09-21 08:43:47 +0900

    🌧 :: 시간 기본 값 추가

--- src/main/java/com/example/asterbackend/AsterBackendApplication.java
+++ src/main/java/com/example/asterbackend/AsterBackendApplication.java
@@ -2,9 +2,19 @@ package com.example.asterbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AsterBackendApplication {
+
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AsterBackendApplication.class, args);
