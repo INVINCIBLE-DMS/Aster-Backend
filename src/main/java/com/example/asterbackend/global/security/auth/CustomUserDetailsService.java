d021aaea855e35219b9834f962985a41d1089b25
user <b57062562@gmail.com>
2023-09-20 15:20:01 +0900

    🌧 :: CustomUseDetailsService 추가

---
+++ src/main/java/com/example/asterbackend/global/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.asterbackend.global.security.auth;
+
+import com.example.asterbackend.domain.user.entity.User;
+import com.example.asterbackend.domain.user.repository.UserRepository;
+import com.example.asterbackend.global.exception.user.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String nickname) {
+
+        User user = userRepository.findByNickname(nickname)
+                .orElseThrow(()-> UserNotFoundException.EXCEPTION);
+
+        return new CustomUserDetails(user);
+    }
+
+}
