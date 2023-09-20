bfed3e00b47e1feac0781347080f68bb9721ee0f
user <b57062562@gmail.com>
2023-09-20 14:21:04 +0900

    🌧 :: user entity 추가

--- src/main/java/com/example/asterbackend/domain/user/entity/User.java
+++ src/main/java/com/example/asterbackend/domain/user/entity/User.java
@@ -1,4 +1,20 @@
 package com.example.asterbackend.domain.user.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+
+    @Id
+    private String nickname;
+
+    private String username;
 }
+
