679bcf4ec5d6b75339333e2dbeee7caaf1f3db61
4mjeo <4mj.eo@dsm.hs.kr>
2023-09-21 23:09:11 +0900

    🌱:: AuthController

---
+++ src/main/java/com/example/asterbackend/domain/user/presentation/AuthController.java
@@ -0,0 +1,23 @@
+package com.example.asterbackend.domain.user.presentation;
+
+import com.example.asterbackend.domain.user.presentation.dto.request.SignupRequest;
+import com.example.asterbackend.domain.user.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signup(@RequestBody @Valid SignupRequest request){
+        authService.signup(request);
+    }
+}
