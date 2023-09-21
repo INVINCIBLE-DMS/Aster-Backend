668906ccfd5dd4071d8c338836e768a259540f88
4mjeo <4mj.eo@dsm.hs.kr>
2023-09-21 22:53:16 +0900

    🐛 :: 서비스 수정

--- src/main/java/com/example/asterbackend/domain/user/service/AuthService.java
+++ src/main/java/com/example/asterbackend/domain/user/service/AuthService.java
@@ -1,5 +1,7 @@
 package com.example.asterbackend.domain.user.service;
 
+import com.example.asterbackend.domain.user.presentation.dto.request.SignupRequest;
+
 public interface AuthService {
     void signup(SignupRequest request);
 }
--- src/main/java/com/example/asterbackend/domain/user/service/AuthServiceImpl.java
+++ src/main/java/com/example/asterbackend/domain/user/service/AuthServiceImpl.java
@@ -1,29 +1,30 @@
 package com.example.asterbackend.domain.user.service;
 
+import com.example.asterbackend.domain.user.entity.User;
+import com.example.asterbackend.domain.user.presentation.dto.request.SignupRequest;
+import com.example.asterbackend.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-public class AuthServiceImpl {
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService{
 
-    @Service
-    @Transactional
-    @RequiredArgsConstructor
-    public class AuthServiceImpl implements AuthService{
+    private final UserRepository userRepository;
 
-        private final UserRepository userRepository;
+    @Override
+    public void signup(SignupRequest request) {
 
-        @Override
-        public void signup(SignupRequest request) {
+        User user = User.builder()
+                .username(request.getUsername())
+                .nickname(request.getNickname())
+                .sex(request.getSex())
+                .part(request.getPart())
+                .build();
 
-            User user = User.builder()
-                    .username(request.getUsername())
-                    .nickname(request.getNickname())
-                    .sex(request.getSex())
-                    .part(request.getPart())
-                    .build();
-
-            userRepository.save(user);
+        userRepository.save(user);
         }
     }
-}
+
