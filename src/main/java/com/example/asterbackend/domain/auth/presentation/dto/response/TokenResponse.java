272a83e2f1e5f51ac247e390c65e7158217db1b2
4mjeo <4mj.eo@dsm.hs.kr>
2023-09-22 16:42:20 +0900

    🌱:: TokenResponse 추가

---
+++ src/main/java/com/example/asterbackend/domain/auth/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package com.example.asterbackend.domain.auth.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TokenResponse {
+
+    String refreshToken;
+    String accessToken;
+}
