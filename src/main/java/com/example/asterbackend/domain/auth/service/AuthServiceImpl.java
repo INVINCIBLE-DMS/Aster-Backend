a2e48f6d54134070063d4d5f82eb787520677f71
4mjeo <4mj.eo@dsm.hs.kr>
2023-09-22 16:21:24 +0900

    🌱:: 회원가입 수정 및 MyInfo 추가

--- src/main/java/com/example/asterbackend/domain/auth/service/AuthServiceImpl.java
+++ src/main/java/com/example/asterbackend/domain/auth/service/AuthServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.asterbackend.domain.auth.service;
 
+import com.example.asterbackend.domain.auth.presentation.dto.response.MyInfoResponse;
 import com.example.asterbackend.domain.user.entity.User;
 import com.example.asterbackend.domain.auth.presentation.dto.request.SignupRequest;
+import com.example.asterbackend.domain.user.facade.UserFacade;
 import com.example.asterbackend.domain.user.repository.UserRepository;
-import com.example.asterbackend.global.security.jwt.JwtTokenProvider;
+import com.example.asterbackend.global.exception.user.UserExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +16,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthServiceImpl implements AuthService{
 
     private final UserRepository userRepository;
-    private final JwtTokenProvider jwtTokenProvider;
+    private final UserFacade userFacade;
+
+    public void duplicateNickname(String nickname) {
+
+        if(userRepository.findByNickname(nickname).isPresent()) {
+            throw UserExistsException.EXCEPTION;
+        }
+    }
 
     @Override
     public void signup(SignupRequest request) {
+
+        duplicateNickname(request.getNickname());
 
         User user = User.builder()
                 .username(request.getUsername())
@@ -28,5 +39,13 @@ public class AuthServiceImpl implements AuthService{
 
         userRepository.save(user);
         }
+
+    @Override
+    public MyInfoResponse getMyInfo() {
+
+        User user = userFacade.getCurrentUser();
+
+        return new MyInfoResponse(user);
     }
+}
 
