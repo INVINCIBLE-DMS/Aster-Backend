9b6215c7fb6fac2d4b1e5e3317587c08ef534677
JungHyun <b57062562@gmail.com>
2023-09-23 22:25:39 +0900

    🌨 :: List 의존성 추가

--- src/main/java/com/example/asterbackend/domain/board/repository/BoardRepository.java
+++ src/main/java/com/example/asterbackend/domain/board/repository/BoardRepository.java
@@ -3,5 +3,7 @@ package com.example.asterbackend.domain.board.repository;
 import com.example.asterbackend.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
 }
