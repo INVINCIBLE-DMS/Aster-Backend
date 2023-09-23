05400b5decbcfa56155863a1980c876374c10993
JungHyun <b57062562@gmail.com>
2023-09-23 22:24:54 +0900

    ♻️ :: write에서 create로 변경

--- src/main/java/com/example/asterbackend/domain/board/presentation/BoardController.java
+++ src/main/java/com/example/asterbackend/domain/board/presentation/BoardController.java
@@ -1,14 +1,14 @@
 package com.example.asterbackend.domain.board.presentation;
 
 import com.example.asterbackend.domain.board.presentation.dto.request.CreateBoardRequest;
+import com.example.asterbackend.domain.board.presentation.dto.response.BoardListResponse;
 import com.example.asterbackend.domain.board.service.CreateBoardService;
+import com.example.asterbackend.domain.board.service.QueryBoardListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +17,16 @@ public class BoardController {
 
     private final CreateBoardService createBoardService;
 
+    private final QueryBoardListService queryBoardListService;
+
     @PostMapping()
-    public void writeBoard(@RequestBody @Valid CreateBoardRequest createBoardRequest) {
+    public void createBoard(@RequestBody @Valid CreateBoardRequest createBoardRequest) {
         createBoardService.createBoard(createBoardRequest);
+    }
+
+    @GetMapping()
+    public List<BoardListResponse> queryBoardList() {
+        return queryBoardListService.queryBoardList();
     }
 
 }
