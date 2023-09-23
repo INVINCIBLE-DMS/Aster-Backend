36089270f601c158f830f32ebc103ca29301289c
JungHyun <b57062562@gmail.com>
2023-09-23 22:25:56 +0900

    🌨 :: BoardResponse 추가

---
+++ src/main/java/com/example/asterbackend/domain/board/presentation/dto/response/BoardListResponse.java
@@ -0,0 +1,25 @@
+package com.example.asterbackend.domain.board.presentation.dto.response;
+
+import com.example.asterbackend.domain.board.entity.Board;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class BoardListResponse {
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime writeTime;
+
+    public BoardListResponse(Board board) {
+        this.title = board.getTitle();
+        this.content = board.getContent();
+        this.writeTime = board.getWriteTime();
+    }
+
+}
