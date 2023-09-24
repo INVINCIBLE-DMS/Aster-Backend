4f50e74a7193fab0c65b5bbe71b9b57e94830c3c
user <b57062562@gmail.com>
2023-09-24 22:20:27 +0900

    ♻️ :: config 수정

--- src/main/java/com/example/asterbackend/global/config/SecurityConfig.java
+++ src/main/java/com/example/asterbackend/global/config/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -44,7 +42,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/auth").permitAll()
-                .antMatchers("/Feed/**").authenticated()
+                .antMatchers("/feed/**").authenticated()
                 .anyRequest().permitAll()
 
                 .and()
