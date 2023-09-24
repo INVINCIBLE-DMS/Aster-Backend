b9b70650a670fd3f8a6452501ccc34876f37bca7
user <b57062562@gmail.com>
2023-09-25 08:19:36 +0900

    🐛:: 좋아요 추가 로직 변경

--- src/main/java/com/example/asterbackend/domain/feed/service/AddLikeService.java
+++ src/main/java/com/example/asterbackend/domain/feed/service/AddLikeService.java
@@ -3,12 +3,12 @@ package com.example.asterbackend.domain.feed.service;
 import com.example.asterbackend.domain.feed.entity.Feed;
 import com.example.asterbackend.domain.feed.entity.Like;
 import com.example.asterbackend.domain.feed.facade.FeedFacade;
-import com.example.asterbackend.domain.feed.facade.FeedFacade;
 import com.example.asterbackend.domain.feed.repository.LikeRepository;
 import com.example.asterbackend.domain.user.entity.User;
 import com.example.asterbackend.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,18 +20,19 @@ public class AddLikeService {
 
     private final LikeRepository likeRepository;
 
+    @Transactional
     public void addLike(Long feedId) {
         Feed feed = feedFacade.currentFeed(feedId);
 
         User user = userFacade.getCurrentUser();
 
         if (likeRepository.existsByFeedIdAndNickname(feedId, user.getNickname())) {
-            feed.addLike();
+            feed.cancelLike();
 
             likeRepository.deleteByFeedIdAndNickname(feedId, user.getNickname());
         }
         else {
-            feed.cancelLike();
+            feed.addLike();
 
             likeRepository.save(
                     Like.builder()
