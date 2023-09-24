c4fd2abe2d80ed7ebfdd3dd7f73515b5644599c2
user <b57062562@gmail.com>
2023-09-25 08:25:26 +0900

    🌨 :: @Transactional 추가

--- src/main/java/com/example/asterbackend/domain/feed/service/UpdateFeedService.java
+++ src/main/java/com/example/asterbackend/domain/feed/service/UpdateFeedService.java
@@ -5,6 +5,7 @@ import com.example.asterbackend.domain.feed.facade.FeedFacade;
 import com.example.asterbackend.domain.feed.presentation.dto.request.UpdateFeedRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class UpdateFeedService {
 
     private final FeedFacade feedFacade;
 
+    @Transactional
     public void updateFeed(UpdateFeedRequest updateFeedRequest, Long feedId) {
         Feed feed = feedFacade.currentFeed(feedId);
 
