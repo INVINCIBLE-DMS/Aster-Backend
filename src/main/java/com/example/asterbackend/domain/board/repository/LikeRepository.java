362a533f76358d5739fa95e369e5e9e64a2c3460
user <b57062562@gmail.com>
2023-09-24 17:20:06 +0900

    🌨 :: 좋아요 존재 여부 확인 메서드 추가

--- src/main/java/com/example/asterbackend/domain/board/repository/LikeRepository.java
+++ src/main/java/com/example/asterbackend/domain/board/repository/LikeRepository.java
@@ -4,4 +4,5 @@ import com.example.asterbackend.domain.board.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    boolean existsByBoardIdAndNickname(Long boardId, String nickname);
 }
