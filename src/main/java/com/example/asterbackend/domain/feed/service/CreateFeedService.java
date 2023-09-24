a9c6dc7476dbae07056aa8357f653bb4cc482b5b
user <b57062562@gmail.com>
2023-09-24 22:21:35 +0900

    ♻️ :: user값 저장

--- src/main/java/com/example/asterbackend/domain/feed/service/CreateFeedService.java
+++ src/main/java/com/example/asterbackend/domain/feed/service/CreateFeedService.java
@@ -3,6 +3,8 @@ package com.example.asterbackend.domain.feed.service;
 import com.example.asterbackend.domain.feed.entity.Feed;
 import com.example.asterbackend.domain.feed.presentation.dto.request.CreateFeedRequest;
 import com.example.asterbackend.domain.feed.repository.FeedRepository;
+import com.example.asterbackend.domain.user.entity.User;
+import com.example.asterbackend.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,17 @@ public class CreateFeedService {
 
     private final FeedRepository feedRepository;
 
+    private final UserFacade userFacade;
+
     public void createFeed(CreateFeedRequest request) {
         LocalDateTime now = LocalDateTime.now();
+
+        User user = userFacade.getCurrentUser();
 
         feedRepository.save(
                 Feed.builder()
                         .content(request.getContent())
+                        .user(user)
                         .writeTime(now)
                         .build());
     }
