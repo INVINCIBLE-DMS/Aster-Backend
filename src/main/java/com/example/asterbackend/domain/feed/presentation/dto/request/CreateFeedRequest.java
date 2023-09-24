188ca531ea7aa254577f194f9763d4a38e69efb3
user <b57062562@gmail.com>
2023-09-24 22:21:48 +0900

    ♻️ :: 이상한거 제거

--- src/main/java/com/example/asterbackend/domain/feed/presentation/dto/request/CreateFeedRequest.java
+++ src/main/java/com/example/asterbackend/domain/feed/presentation/dto/request/CreateFeedRequest.java
@@ -4,14 +4,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateFeedRequest {
-
     private String content;
-
-    private LocalDateTime writeTime;
-
 }
