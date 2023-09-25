e41657bd7b4fa298dd8f17fd75af6c5b316abc00
user <b57062562@gmail.com>
2023-09-25 09:08:53 +0900

    🌨 :: comment dto 추가

---
+++ src/main/java/com/example/asterbackend/domain/comment/presentation/request/CreateAndUpdateCommentRequest.java
@@ -0,0 +1,11 @@
+package com.example.asterbackend.domain.comment.presentation.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateAndUpdateCommentRequest {
+    private String content;
+}
