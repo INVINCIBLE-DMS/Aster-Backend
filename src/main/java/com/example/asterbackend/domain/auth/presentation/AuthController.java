c404e3eac8be601349c19a5ece72a0c4206ca01f
4mjeo <4mj.eo@dsm.hs.kr>
2023-09-25 17:14:53 +0900

    ♻️ :: controller 수정

--- src/main/java/com/example/asterbackend/domain/auth/presentation/AuthController.java
+++ src/main/java/com/example/asterbackend/domain/auth/presentation/AuthController.java
@@ -3,7 +3,9 @@ package com.example.asterbackend.domain.auth.presentation;
 import com.example.asterbackend.domain.auth.presentation.dto.request.SignupRequest;
 import com.example.asterbackend.domain.auth.presentation.dto.response.MyInfoResponse;
 import com.example.asterbackend.domain.auth.presentation.dto.response.TokenResponse;
-import com.example.asterbackend.domain.auth.service.AuthService;
+import com.example.asterbackend.domain.auth.service.MyInfoService;
+import com.example.asterbackend.domain.auth.service.SignupService;
+import com.example.asterbackend.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,16 +17,19 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
+    private final SignupService signupService;
+    private final MyInfoService myInfoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public TokenResponse signup(@RequestBody @Valid SignupRequest request){
-        return authService.signup(request);
+    public TokenResponse signup(@RequestBody @Valid SignupRequest request) {
+        return signupService.signup(request);
     }
 
     @GetMapping("/myInfo")
     public MyInfoResponse getMyInfo() {
-        return authService.getMyInfo();
+        return myInfoService.getMyInfo();
     }
+
+
 }
