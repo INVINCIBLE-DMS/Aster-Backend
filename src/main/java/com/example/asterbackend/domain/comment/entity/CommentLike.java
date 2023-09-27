84862d3c20a5a0448090fe3dba451f66ef35101e
user <b57062562@gmail.com>
2023-09-27 20:44:25 +0900

    🌨 :: 댓글 좋아요 추가

---
+++ src/main/java/com/example/asterbackend/domain/comment/entity/CommentLike.java
@@ -0,0 +1,25 @@
+package com.example.asterbackend.domain.comment.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "comment_id", nullable = false)
+    private Comment comment;
+
+}
