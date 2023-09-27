df00bd0f4eb35284e5f0206ef374f38205a6d558
user <b57062562@gmail.com>
2023-09-27 20:33:50 +0900

    ♻️ :: comment 조회 메서드 추가

--- src/main/java/com/example/asterbackend/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/asterbackend/domain/comment/repository/CommentRepository.java
@@ -3,8 +3,8 @@ package com.example.asterbackend.domain.comment.repository;
 import com.example.asterbackend.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Comment findByFeedIdAndNickname(Long feedId, String nickname);
+import java.util.List;
 
-    void deleteByFeedIdAndNickname(Long feedId, String nickname);
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByFeedId(Long feedId);
 }
