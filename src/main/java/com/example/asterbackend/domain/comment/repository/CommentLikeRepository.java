a7d98e101467c7d16d50e71eb337c749eb2a4e31
user <b57062562@gmail.com>
2023-09-27 20:48:19 +0900

    🌨 :: 댓글 좋아요 repository 추가

---
+++ src/main/java/com/example/asterbackend/domain/comment/repository/CommentLikeRepository.java
@@ -0,0 +1,8 @@
+package com.example.asterbackend.domain.comment.repository;
+
+import com.example.asterbackend.domain.comment.entity.CommentLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+    boolean existsByCommentIdAndNickname(Long commentId, String nickname);
+}
