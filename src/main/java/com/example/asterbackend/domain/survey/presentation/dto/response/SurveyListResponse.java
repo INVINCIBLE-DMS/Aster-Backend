109ecd157ee5681f1f89831a389bf55d8834e913
user <b57062562@gmail.com>
2023-09-28 23:02:36 +0900

    🌨 :: 설문 조회 response 수정

--- src/main/java/com/example/asterbackend/domain/survey/presentation/dto/response/SurveyListResponse.java
+++ src/main/java/com/example/asterbackend/domain/survey/presentation/dto/response/SurveyListResponse.java
@@ -8,11 +8,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public class SurveyListResponse {
 
+    private Long id;
+
     private String content;
 
     private int agreeCnt;
 
     public SurveyListResponse(Survey survey) {
+        this.id = survey.getId();
         this.content = survey.getContent();
         this.agreeCnt = survey.getAgreeCnt();
     }
