121c9879915d59496c9376a6464f7e92119dadd8
user <b57062562@gmail.com>
2023-09-28 22:07:21 +0900

    🌨 :: 설문 댓글 작성 기능 추가

---
+++ src/main/java/com/example/asterbackend/domain/survey/service/CreateSurveyCommentService.java
@@ -0,0 +1,40 @@
+package com.example.asterbackend.domain.survey.service;
+
+import com.example.asterbackend.domain.comment.entity.Comment;
+import com.example.asterbackend.domain.comment.presentation.request.CreateAndUpdateCommentRequest;
+import com.example.asterbackend.domain.comment.repository.CommentRepository;
+import com.example.asterbackend.domain.feed.entity.Feed;
+import com.example.asterbackend.domain.feed.facade.FeedFacade;
+import com.example.asterbackend.domain.survey.entity.Survey;
+import com.example.asterbackend.domain.survey.entity.SurveyComment;
+import com.example.asterbackend.domain.survey.facade.SurveyFacade;
+import com.example.asterbackend.domain.survey.repository.SurveyCommentRepository;
+import com.example.asterbackend.domain.user.entity.User;
+import com.example.asterbackend.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateSurveyCommentService {
+
+    private final SurveyCommentRepository surveyCommentRepository;
+
+    private final SurveyFacade surveyFacade;
+
+    private final UserFacade userFacade;
+
+    public void createSurveyComment(Long surveyId, CreateAndUpdateCommentRequest request) {
+        Survey survey = surveyFacade.currentSurvey(surveyId);
+
+        User user = userFacade.getCurrentUser();
+
+        surveyCommentRepository.save(
+                SurveyComment.builder()
+                        .content(request.getContent())
+                        .nickname(user.getNickname())
+                        .survey(survey)
+                        .build());
+    }
+
+}
