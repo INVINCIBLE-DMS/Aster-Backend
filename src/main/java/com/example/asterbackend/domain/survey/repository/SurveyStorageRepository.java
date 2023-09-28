6d2dc804fe92ff252233aaf7aa2f8c71e704709a
user <b57062562@gmail.com>
2023-09-28 23:28:41 +0900

    🌨 :: 설문조사 저장소 repository 추가

---
+++ src/main/java/com/example/asterbackend/domain/survey/repository/SurveyStorageRepository.java
@@ -0,0 +1,7 @@
+package com.example.asterbackend.domain.survey.repository;
+
+import com.example.asterbackend.domain.survey.entity.SurveyStorage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SurveyStorageRepository extends JpaRepository<SurveyStorage, Long> {
+}
