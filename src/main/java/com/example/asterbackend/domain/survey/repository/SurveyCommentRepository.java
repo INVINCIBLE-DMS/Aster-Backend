367f1281be49a6f7b280e6981c642130f69df2e9
user <b57062562@gmail.com>
2023-09-28 22:25:56 +0900

    🌨 :: 설문 댓글 조회 메서드 추가

--- src/main/java/com/example/asterbackend/domain/survey/repository/SurveyCommentRepository.java
+++ src/main/java/com/example/asterbackend/domain/survey/repository/SurveyCommentRepository.java
@@ -3,5 +3,8 @@ package com.example.asterbackend.domain.survey.repository;
 import com.example.asterbackend.domain.survey.entity.SurveyComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SurveyCommentRepository extends JpaRepository<SurveyComment, Long> {
+    List<SurveyComment> findAllBySurveyId(Long surveyId);
 }
