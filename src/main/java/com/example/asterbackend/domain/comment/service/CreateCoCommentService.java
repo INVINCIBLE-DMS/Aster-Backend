5f20f90abdcd877f7f3c0e3211e5a63c2d43d016
user <b57062562@gmail.com>
2023-09-28 16:38:07 +0900

    🌨 :: 대댓글 작성 기능 추가

---
+++ src/main/java/com/example/asterbackend/domain/comment/service/CreateCoCommentService.java
@@ -0,0 +1,41 @@
+package com.example.asterbackend.domain.comment.service;
+
+import com.example.asterbackend.domain.comment.entity.CoComment;
+import com.example.asterbackend.domain.comment.entity.Comment;
+import com.example.asterbackend.domain.comment.facade.CommentFacade;
+import com.example.asterbackend.domain.comment.presentation.request.CreateAndUpdateCommentRequest;
+import com.example.asterbackend.domain.comment.repository.CoCommentRepository;
+import com.example.asterbackend.domain.comment.repository.CommentRepository;
+import com.example.asterbackend.domain.feed.entity.Feed;
+import com.example.asterbackend.domain.feed.facade.FeedFacade;
+import com.example.asterbackend.domain.user.entity.User;
+import com.example.asterbackend.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateCoCommentService {
+
+    private final CoCommentRepository coCommentRepository;
+
+    private final CommentFacade commentFacade;
+
+    private final UserFacade userFacade;
+
+    public void createCoComment(Long commentId, CreateAndUpdateCommentRequest request) {
+        Comment comment = commentFacade.currentComment(commentId);
+
+        User user = userFacade.getCurrentUser();
+
+        CoComment cocomment = CoComment.builder()
+                .content(request.getContent())
+                .nickname(user.getNickname())
+                .comment(comment)
+                .build();
+
+        coCommentRepository.save(cocomment);
+        comment.addComment(cocomment);
+    }
+
+}
