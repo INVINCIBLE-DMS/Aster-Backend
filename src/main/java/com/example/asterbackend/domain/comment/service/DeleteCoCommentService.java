5977c13d4cd1dda1984c23b00222b62a29a75520
JungHyun <b57062562@gmail.com>
2023-09-28 17:36:05 +0900

    🌨 :: 대댓글 삭제 기능 추가

---
+++ src/main/java/com/example/asterbackend/domain/comment/service/DeleteCoCommentService.java
@@ -0,0 +1,15 @@
+package com.example.asterbackend.domain.comment.service;
+
+import com.example.asterbackend.domain.comment.repository.CoCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteCoCommentService {
+    private final CoCommentRepository coCommentRepository;
+
+    public void deleteComment(Long coCommentId) {
+        coCommentRepository.deleteById(coCommentId);
+    }
+}
