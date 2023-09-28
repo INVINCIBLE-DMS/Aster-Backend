ea2afacd8f135d442e58cb7e2c6d575b174960c5
JungHyun <b57062562@gmail.com>
2023-09-28 17:41:23 +0900

    🌨 :: 대댓글 조회 기능 추가

---
+++ src/main/java/com/example/asterbackend/domain/comment/service/QueryCoCommentService.java
@@ -0,0 +1,25 @@
+package com.example.asterbackend.domain.comment.service;
+
+import com.example.asterbackend.domain.comment.presentation.response.QueryCoCommentResponse;
+import com.example.asterbackend.domain.comment.presentation.response.QueryCommentResponse;
+import com.example.asterbackend.domain.comment.repository.CoCommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QueryCoCommentService {
+
+    private final CoCommentRepository coCommentRepository;
+
+    public List<QueryCoCommentResponse> queryCoComment(Long commentId) {
+        return coCommentRepository.findAllByCommentId(commentId)
+                .stream()
+                .map(QueryCoCommentResponse::new)
+                .collect(Collectors.toList());
+    }
+
+}
