e94bcd713c63a6182d9911ab6588eb5c5a52dbce
user <b57062562@gmail.com>
2023-10-01 01:17:19 +0900

    ♻️ :: access = AccessLevel.PROTECTED추가

--- src/main/java/com/example/asterbackend/domain/survey/presentation/dto/request/AnswerSurveyRequest.java
+++ src/main/java/com/example/asterbackend/domain/survey/presentation/dto/request/AnswerSurveyRequest.java
@@ -2,11 +2,12 @@ package com.example.asterbackend.domain.survey.presentation.dto.request;
 
 import com.example.asterbackend.domain.survey.entity.type.AnswerType;
 import com.example.asterbackend.domain.survey.entity.type.SurveyType;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AnswerSurveyRequest {
 
     private SurveyType surveyType;
--- src/main/java/com/example/asterbackend/domain/survey/presentation/dto/request/SurveyRequest.java
+++ src/main/java/com/example/asterbackend/domain/survey/presentation/dto/request/SurveyRequest.java
@@ -1,11 +1,12 @@
 package com.example.asterbackend.domain.survey.presentation.dto.request;
 
 import com.example.asterbackend.domain.survey.entity.type.SurveyType;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SurveyRequest {
 
     private String content;
