89131aa9d876a0bf741a137311ef0649a9a3e5ef
user <b57062562@gmail.com>
2023-10-01 00:06:55 +0900

    ♻️ :: 설문조사 조회 메서드 수정

--- src/main/java/com/example/asterbackend/domain/survey/repository/CustomSurveyStorageRepository.java
+++ src/main/java/com/example/asterbackend/domain/survey/repository/CustomSurveyStorageRepository.java
@@ -1,10 +1,10 @@
 package com.example.asterbackend.domain.survey.repository;
 
-import com.example.asterbackend.domain.survey.entity.Survey;
+import com.example.asterbackend.domain.survey.entity.SurveyStorage;
 import com.example.asterbackend.domain.survey.entity.type.SurveyType;
 
 import java.util.List;
 
 public interface CustomSurveyStorageRepository {
-    List<Survey> findEnumSurvey(SurveyType surveyType);
+    List<SurveyStorage> findEnumSurvey(SurveyType surveyType);
 }
--- src/main/java/com/example/asterbackend/domain/survey/repository/CustomSurveyStorageRepositoryImpl.java
+++ src/main/java/com/example/asterbackend/domain/survey/repository/CustomSurveyStorageRepositoryImpl.java
@@ -1,14 +1,16 @@
 package com.example.asterbackend.domain.survey.repository;
 
-import com.example.asterbackend.domain.survey.entity.Survey;
+import com.example.asterbackend.domain.survey.entity.SurveyStorage;
 import com.example.asterbackend.domain.survey.entity.type.SurveyType;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static com.example.asterbackend.domain.survey.entity.QSurvey.survey;
+import static com.example.asterbackend.domain.survey.entity.QSurveyStorage.surveyStorage;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,11 +18,17 @@ public class CustomSurveyStorageRepositoryImpl implements CustomSurveyStorageRep
 
     private final JPAQueryFactory queryFactory;
 
-    public List<Survey> findEnumSurvey(SurveyType surveyType) {
-        return queryFactory
-                .selectFrom(survey)
-                .where(survey.surveyType.eq(surveyType))
+    public List<SurveyStorage> findEnumSurvey(SurveyType surveyType) {
+        List<SurveyStorage> surveys = queryFactory
+                .selectFrom(surveyStorage)
+                .where(surveyStorage.surveyType.eq(surveyType))
                 .fetch();
+
+        Collections.shuffle(surveys);
+
+        return surveys.stream()
+                .limit(3)
+                .collect(Collectors.toList());
     }
 
 }
