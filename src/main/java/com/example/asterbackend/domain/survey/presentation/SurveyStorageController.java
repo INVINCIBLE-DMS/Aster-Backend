5ab45b2fcb48d788ba2ba1fd5a8d39a0cf5b4317
user <b57062562@gmail.com>
2023-10-01 01:11:48 +0900

    ♻️ :: 구조 수정

--- src/main/java/com/example/asterbackend/domain/survey/presentation/SurveyStorageController.java
+++ src/main/java/com/example/asterbackend/domain/survey/presentation/SurveyStorageController.java
@@ -1,14 +1,14 @@
 package com.example.asterbackend.domain.survey.presentation;
 
+import com.example.asterbackend.domain.survey.entity.SurveyStorage;
 import com.example.asterbackend.domain.survey.presentation.dto.request.SurveyRequest;
+import com.example.asterbackend.domain.survey.service.QueryNewSurveyService;
 import com.example.asterbackend.domain.survey.service.SaveSurveyStorageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +17,16 @@ public class SurveyStorageController {
 
     private final SaveSurveyStorageService saveSurveyStorageService;
 
+    private final QueryNewSurveyService queryNewSurveyService;
+
     @PostMapping()
     public void saveSurveyStorage(@RequestBody @Valid SurveyRequest request) {
         saveSurveyStorageService.saveSurveyStorage(request);
     }
+
+    @GetMapping("/new")
+    public List<SurveyStorage> queryNewSurvey() {
+        return queryNewSurveyService.queryNewSurvey();
+    }
+
 }
