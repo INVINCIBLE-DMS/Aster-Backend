844d2a40a903aa4466af29da2d47563186e69507
user <b57062562@gmail.com>
2023-10-01 00:07:11 +0900

    🌨 :: 설문조사 생성 기능 추가

--- src/main/java/com/example/asterbackend/domain/survey/service/QueryNewSurveyService.java
+++ src/main/java/com/example/asterbackend/domain/survey/service/QueryNewSurveyService.java
@@ -5,17 +5,26 @@ import com.example.asterbackend.domain.survey.repository.SurveyStorageRepository
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.asterbackend.domain.survey.entity.type.SurveyType.*;
+
 @Service
 @RequiredArgsConstructor
 public class QueryNewSurveyService {
 
-   // private final Random random;
-
     private final SurveyStorageRepository surveyStorageRepository;
 
-    public SurveyStorage queryNewSurvey(Long surveyId) {
-        return surveyStorageRepository.findById(surveyId)
-                .orElseThrow();
+    public List<SurveyStorage> queryNewSurvey(Long surveyId) {
+        List<SurveyStorage> surveyList = new ArrayList<>();
+
+        surveyList.addAll(surveyStorageRepository.findEnumSurvey(SOCIAL_TYPE));
+        surveyList.addAll(surveyStorageRepository.findEnumSurvey(KNOWLEDGE_TYPE));
+        surveyList.addAll(surveyStorageRepository.findEnumSurvey(EMOTION_TYPE));
+        surveyList.addAll(surveyStorageRepository.findEnumSurvey(DECISION_TYPE));
+
+        return surveyList;
     }
 
 }
