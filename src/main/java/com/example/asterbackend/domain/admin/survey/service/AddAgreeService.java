977bc7b9c51ce79609aef8240b70e38875f6dc7f
user <b57062562@gmail.com>
2023-10-01 21:11:12 +0900

    🌨 :: secretKey 추가

--- src/main/java/com/example/asterbackend/domain/admin/survey/service/AddAgreeService.java
+++ src/main/java/com/example/asterbackend/domain/admin/survey/service/AddAgreeService.java
@@ -1,11 +1,11 @@
 package com.example.asterbackend.domain.admin.survey.service;
 
-import com.example.asterbackend.domain.admin.survey.repository.SurveyRepository;
-import com.example.asterbackend.domain.user.survey.entity.Agree;
+import com.example.asterbackend.domain.admin.survey.entity.Agree;
 import com.example.asterbackend.domain.admin.survey.entity.Survey;
 import com.example.asterbackend.domain.admin.survey.entity.SurveyStorage;
 import com.example.asterbackend.domain.admin.survey.facade.SurveyFacade;
 import com.example.asterbackend.domain.admin.survey.repository.AgreeRepository;
+import com.example.asterbackend.domain.admin.survey.repository.SurveyRepository;
 import com.example.asterbackend.domain.admin.survey.repository.SurveyStorageRepository;
 import com.example.asterbackend.domain.user.user.entity.User;
 import com.example.asterbackend.domain.user.user.facade.UserFacade;
