770fbfca97bc2c92c95ff85f9be1756d5dbe912b
user <b57062562@gmail.com>
2023-10-01 21:14:03 +0900

    🌨 :: admin entity 추가

---
+++ src/main/java/com/example/asterbackend/domain/admin/auth/Admin.java
@@ -0,0 +1,22 @@
+package com.example.asterbackend.domain.admin.auth;
+
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin {
+
+    @Id
+    private String username;
+
+    @Value("${key.secretKey}")
+    private String secretKey;
+
+}
