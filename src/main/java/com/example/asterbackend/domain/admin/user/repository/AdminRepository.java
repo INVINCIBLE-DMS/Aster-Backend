50e497e276cdf037f333114701685317e2cd9d3b
tae.y <0211ilyoil@gmail.com>
2023-10-01 23:04:08 +0900

    🌧 :: S3 설정 추가

--- src/main/java/com/example/asterbackend/domain/admin/user/repository/AdminRepository.java
+++ src/main/java/com/example/asterbackend/domain/admin/user/repository/AdminRepository.java
@@ -1,6 +1,7 @@
 package com.example.asterbackend.domain.admin.user.repository;
 
-import com.example.asterbackend.domain.admin.user.entity.Admin;
+
+import com.example.asterbackend.domain.admin.auth.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, String> {
