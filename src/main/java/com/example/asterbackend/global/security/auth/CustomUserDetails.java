7ae0aedf727823edbdab98eb058637f3166ee41a
JungHyun <b57062562@gmail.com>
2023-10-04 21:54:42 +0900

    🐛:: 태양아 지렸다

--- src/main/java/com/example/asterbackend/global/security/auth/CustomUserDetails.java
+++ src/main/java/com/example/asterbackend/global/security/auth/CustomUserDetails.java
@@ -17,7 +17,7 @@ public record CustomUserDetails(User user) implements UserDetails {
     }
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority("ROLE_"+user.getNickname().toString())));
+        return new ArrayList<>(Collections.singleton(new SimpleGrantedAuthority("ROLE_"+user.getRole().toString())));
     }
 
     @Override
