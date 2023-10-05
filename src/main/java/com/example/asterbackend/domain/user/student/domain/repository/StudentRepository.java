d673805286c2cd77ffecdf60eb95cf4612b77f94
JungHyun <b57062562@gmail.com>
2023-10-05 22:20:10 +0900

    🌱:: 구조 변경

--- src/main/java/com/example/asterbackend/domain/user/student/domain/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.asterbackend.domain.user.student.domain.repository;
-
-import com.example.asterbackend.domain.user.student.domain.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
