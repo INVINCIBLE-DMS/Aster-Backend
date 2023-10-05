9c7d797f022c4e11eec1a3d7fa5f7708d8f955a8
JungHyun <b57062562@gmail.com>
2023-10-05 22:21:21 +0900

    🌨 :: 학생 저장 및 삭제 기능 추가

--- src/main/java/com/example/asterbackend/domain/user/auth/service/SignupService.java
+++ src/main/java/com/example/asterbackend/domain/user/auth/service/SignupService.java
@@ -2,22 +2,32 @@ package com.example.asterbackend.domain.user.auth.service;
 
 import com.example.asterbackend.domain.user.auth.presentation.dto.request.SignupRequest;
 import com.example.asterbackend.domain.user.auth.presentation.dto.response.TokenResponse;
+import com.example.asterbackend.domain.user.student.repository.StudentRepository;
 import com.example.asterbackend.domain.user.user.entity.User;
 import com.example.asterbackend.domain.user.user.entity.type.Role;
 import com.example.asterbackend.domain.user.user.repository.UserRepository;
+import com.example.asterbackend.global.exception.user.NotStudentException;
 import com.example.asterbackend.global.exception.user.UserExistsException;
 import com.example.asterbackend.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class SignupService {
 
     private final UserRepository userRepository;
+
+    private final StudentRepository studentRepository;
+
     private final JwtTokenProvider jwtTokenProvider;
 
+    @Transactional
     public TokenResponse signup(SignupRequest request) {
+        if(!studentRepository.findByUsername(request.getUsername()).isPresent()) {
+            throw NotStudentException.EXCEPTION;
+        }
 
         if (userRepository.findByNickname(request.getNickname()).isPresent()) {
             throw UserExistsException.EXCEPTION;
@@ -33,6 +43,7 @@ public class SignupService {
                         .role(Role.GENERAL)
                         .build());
 
+        studentRepository.deleteByUsername(request.getUsername());
         return jwtTokenProvider.receiveToken(request.getNickname());
     }
 
