f4d64d6e1481d950f02d7eaa0164c8967cf8231a
user <b57062562@gmail.com>
2023-10-06 19:59:00 +0900

    🌨 :: WhoMath dto 추가

---
+++ src/main/java/com/example/asterbackend/domain/user/user/presentation/dto/request/WhoMatchRequest.java
@@ -0,0 +1,11 @@
+package com.example.asterbackend.domain.user.user.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WhoMatchRequest {
+    private String studentId;
+}
