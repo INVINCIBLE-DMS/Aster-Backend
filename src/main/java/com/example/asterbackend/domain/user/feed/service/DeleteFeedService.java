e704c9da84fdac5e9f3aa70143de6f848c193ef2
tae.y <0211ilyoil@gmail.com>
2023-10-10 20:20:37 +0900

    🌨 :: 작성자만 feed를 지울 수 있도록 변경

--- src/main/java/com/example/asterbackend/domain/user/feed/service/DeleteFeedService.java
+++ src/main/java/com/example/asterbackend/domain/user/feed/service/DeleteFeedService.java
@@ -1,17 +1,36 @@
 package com.example.asterbackend.domain.user.feed.service;
 
+import com.example.asterbackend.domain.user.feed.entity.Feed;
 import com.example.asterbackend.domain.user.feed.repository.FeedRepository;
+import com.example.asterbackend.domain.user.user.entity.User;
+import com.example.asterbackend.domain.user.user.facade.UserFacade;
+import com.example.asterbackend.global.exception.feed.FeedNotFoundException;
+import com.example.asterbackend.global.exception.user.WriterMismatchException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.example.asterbackend.domain.user.user.entity.type.Role.ADMIN;
+import static com.example.asterbackend.global.error.exception.ErrorCode.WRITER_MISMATCH;
 
 @Service
 @RequiredArgsConstructor
 public class DeleteFeedService {
 
     private final FeedRepository feedRepository;
+    private final UserFacade userFacade;
 
     public void deleteFeed(Long feedId) {
-        feedRepository.deleteById(feedId);
+
+        User currentUser = userFacade.getCurrentUser();
+
+        Feed feed = feedRepository.findById(feedId)
+                        .orElseThrow(()-> FeedNotFoundException.EXCEPTION);
+
+        if (!currentUser.equals(feed.getUser())) {
+            throw WriterMismatchException.EXCEPTION;
+        }
+
+        feedRepository.delete(feed);
     }
 
 }
