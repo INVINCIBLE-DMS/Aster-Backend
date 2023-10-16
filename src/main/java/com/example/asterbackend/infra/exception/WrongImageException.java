0f68f57e349c19b035111517e7ec0ce1dc10b043
tae.y <0211ilyoil@gmail.com>
2023-10-16 19:51:44 +0900

    🌨 :: 이미지 관련 exception 추가

---
+++ src/main/java/com/example/asterbackend/infra/exception/WrongImageException.java
@@ -0,0 +1,11 @@
+package com.example.asterbackend.infra.exception;
+
+
+import com.example.asterbackend.global.error.exception.BusinessException;
+import com.example.asterbackend.global.error.exception.ErrorCode;
+
+public class WrongImageException extends BusinessException {
+
+    public static final BusinessException EXCEPTION = new WrongImageException();
+    private WrongImageException() { super(ErrorCode.WRONG_IMAGE);}
+}
