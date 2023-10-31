284799cb1c03535ae18268211471c6d8a11a579d
tae.y <0211ilyoil@gmail.com>
2023-10-31 20:01:55 +0900

    🌨 :: S3 Controller 추가

---
+++ src/main/java/com/example/asterbackend/infra/s3/presentation/S3Controller.java
@@ -0,0 +1,25 @@
+package com.example.asterbackend.infra.s3.presentation;
+
+import com.example.asterbackend.domain.user.feed.presentation.dto.response.ImageUrlResponse;
+import com.example.asterbackend.domain.user.user.facade.UserFacade;
+import com.example.asterbackend.infra.s3.service.CreateImageService;
+import com.example.asterbackend.infra.s3.util.S3Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/image")
+public class S3Controller {
+
+    private final CreateImageService createImageService;
+
+    @PostMapping
+    public ImageUrlResponse createImage(@RequestPart(value = "image", required = false) MultipartFile multipartFiles) {
+        return createImageService.createImage(multipartFiles);
+    }
+}
