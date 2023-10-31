74c39a32d9d5d8448bd95149b521d2df5a2868ce
tae.y <0211ilyoil@gmail.com>
2023-10-31 20:02:26 +0900

    🌨 :: ImageUrlResponse 추가

---
+++ src/main/java/com/example/asterbackend/domain/user/feed/presentation/dto/response/ImageUrlResponse.java
@@ -0,0 +1,13 @@
+package com.example.asterbackend.domain.user.feed.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ImageUrlResponse {
+
+    private String feedImageUrl;
+}
