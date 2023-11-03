2f0091c00cc12fea6fb5bf7949cf51d6a2bb70a1
tae.y <0211ilyoil@gmail.com>
2023-11-03 16:58:49 +0900

    🪓 :: 프로필 사진 업로드 api 삭제

--- src/main/java/com/example/asterbackend/domain/user/user/presentation/UserController.java
+++ src/main/java/com/example/asterbackend/domain/user/user/presentation/UserController.java
@@ -3,12 +3,9 @@ package com.example.asterbackend.domain.user.user.presentation;
 import com.example.asterbackend.domain.user.user.presentation.dto.request.WhoMatchRequest;
 import com.example.asterbackend.domain.user.user.presentation.dto.response.GetNameResponse;
 import com.example.asterbackend.domain.user.user.service.GetNameService;
-import com.example.asterbackend.domain.user.user.service.ProfileUploadService;
 import com.example.asterbackend.domain.user.user.service.WhoMatchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 
@@ -17,17 +14,9 @@ import javax.validation.Valid;
 @RequestMapping("/user")
 public class UserController {
 
-    private final ProfileUploadService profileUploadService;
-
     private final GetNameService getNameService;
 
     private final WhoMatchService whoMatchService;
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/profile")
-    public void upload(@RequestPart(required = false, value = "image") MultipartFile profileImage){
-        profileUploadService.profileUpload(profileImage);
-    }
 
     @GetMapping("/{nickname}")
     public GetNameResponse getName(@PathVariable String nickname) {
