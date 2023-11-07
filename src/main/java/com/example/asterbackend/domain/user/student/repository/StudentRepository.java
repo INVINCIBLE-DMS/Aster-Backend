65228940d0519913deaabcf5368440b6765f1926
dkflfkd53 <b57062562@gmail.com>
2023-11-07 20:01:57 +0900

    ♻️ :: findByStudentId로 변경

--- src/main/java/com/example/asterbackend/domain/user/student/repository/StudentRepository.java
+++ src/main/java/com/example/asterbackend/domain/user/student/repository/StudentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    Optional<Student> findByUsername(String username);
+    Optional<Student> findByStudentId(String studentId);
 
-    void deleteByUsername(String username);
+    void deleteByStudentId(String studentId);
 }
