9ce796afca2475f15e0972217354ab805a6c6421
dkflfkd53 <b57062562@gmail.com>
2023-11-15 22:22:40 +0900

    ♻️ :: 댓글 조회 response 수정

--- src/main/java/com/example/asterbackend/domain/user/comment/presentation/response/QueryCommentResponse.java
+++ src/main/java/com/example/asterbackend/domain/user/comment/presentation/response/QueryCommentResponse.java
@@ -1,6 +1,7 @@
 package com.example.asterbackend.domain.user.comment.presentation.response;
 
 import com.example.asterbackend.domain.user.comment.entity.Comment;
+import com.example.asterbackend.domain.user.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,12 +10,15 @@ import lombok.Getter;
 public class QueryCommentResponse {
     private Long id;
 
+    private User user;
+
     private String content;
 
     private boolean isUpdated;
 
     public QueryCommentResponse(Comment comment) {
         this.id = comment.getId();
+        this.user = comment.getUser();
         this.content = comment.getContent();
         this.isUpdated = comment.isUpdated();
     }
