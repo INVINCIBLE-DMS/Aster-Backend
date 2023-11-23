fe77bab324f464fe368abf469a28c231bbcee712
dkflfkd53 <b57062562@gmail.com>
2023-11-23 23:32:12 +0900

    🌨 :: 반 생성 api 추가

---
+++ src/main/java/com/example/asterbackend/domain/user/schoolClass/service/CreateClassService.java
@@ -0,0 +1,40 @@
+package com.example.asterbackend.domain.user.schoolClass.service;
+
+import com.example.asterbackend.domain.user.schoolClass.entity.SchoolClass;
+import com.example.asterbackend.domain.user.schoolClass.repository.SchoolClassRepository;
+import com.example.asterbackend.domain.user.student.entity.Student;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CreateClassService {
+
+    private final SchoolClassRepository schoolClassRepository;
+
+    public void createAllClass() {
+        List<SchoolClass> schoolClassList = new ArrayList<>();
+
+        //1학년
+        schoolClassList.add(new SchoolClass(1L, 1L, 0));
+        schoolClassList.add(new SchoolClass(1L, 2L, 0));
+        schoolClassList.add(new SchoolClass(1L, 3L, 0));
+        schoolClassList.add(new SchoolClass(1L, 4L, 0));
+
+        //2학년
+        schoolClassList.add(new SchoolClass(2L, 1L, 0));
+        schoolClassList.add(new SchoolClass(2L, 2L, 0));
+        schoolClassList.add(new SchoolClass(2L, 3L, 0));
+        schoolClassList.add(new SchoolClass(2L, 4L, 0));
+
+        //3학년
+        schoolClassList.add(new SchoolClass(3L, 1L, 0));
+        schoolClassList.add(new SchoolClass(3L, 2L, 0));
+        schoolClassList.add(new SchoolClass(3L, 3L, 0));
+        schoolClassList.add(new SchoolClass(3L, 4L, 0));
+
+    }
+}
