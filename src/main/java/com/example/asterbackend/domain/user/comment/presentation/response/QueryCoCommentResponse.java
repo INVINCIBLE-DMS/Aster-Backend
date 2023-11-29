2f37a866aa7ad836348d455df45f6ba4fb62464b
dkflfkd53 <b57062562@gmail.com>
2023-11-29 09:31:48 +0900

    ♻️ :: 대댓글 조회시 유저 반환되도록 수정

--- src/main/java/com/example/asterbackend/domain/user/comment/presentation/response/QueryCoCommentResponse.java
+++ src/main/java/com/example/asterbackend/domain/user/comment/presentation/response/QueryCoCommentResponse.java
@@ -1,6 +1,7 @@
 package com.example.asterbackend.domain.user.comment.presentation.response;
 
 import com.example.asterbackend.domain.user.comment.entity.CoComment;
+import com.example.asterbackend.domain.user.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,8 +12,11 @@ public class QueryCoCommentResponse {
 
     private String content;
 
+    private User user;
+
     public QueryCoCommentResponse(CoComment cocomment) {
         this.id = cocomment.getId();
         this.content = cocomment.getContent();
+        this.user = cocomment.getUser();
     }
 }
