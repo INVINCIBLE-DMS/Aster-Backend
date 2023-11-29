0ec3a2505d811110b412a5fb4d3842dbdbd3de68
dkflfkd53 <b57062562@gmail.com>
2023-11-29 09:01:24 +0900

    ♻️ :: 사탕추가 예외처리 및 변수명 통일

--- src/main/java/com/example/asterbackend/domain/user/user/service/WhoMatchService.java
+++ src/main/java/com/example/asterbackend/domain/user/user/service/WhoMatchService.java
@@ -2,12 +2,10 @@ package com.example.asterbackend.domain.user.user.service;
 
 import com.example.asterbackend.domain.user.schoolClass.entity.SchoolClass;
 import com.example.asterbackend.domain.user.schoolClass.facade.SchoolClassFacade;
-import com.example.asterbackend.domain.user.schoolClass.repository.SchoolClassRepository;
 import com.example.asterbackend.domain.user.user.entity.User;
 import com.example.asterbackend.domain.user.user.facade.UserFacade;
 import com.example.asterbackend.domain.user.user.presentation.dto.request.WhoMatchRequest;
 import com.example.asterbackend.domain.user.user.repository.UserRepository;
-import com.example.asterbackend.global.exception.user.SchoolClassNotFoundException;
 import com.example.asterbackend.global.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,24 +21,27 @@ public class WhoMatchService {
     private final SchoolClassFacade schoolClassFacade;
 
     public int whoMatch(WhoMatchRequest request) {
-        User user = userFacade.getCurrentUser();
+        User me = userFacade.getCurrentUser();
 
-        User who = userRepository.findByUsername(request.getUsername())
+        User user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(()-> UserNotFoundException.EXCEPTION);
 
-        Long grade = Long.parseLong(who.getStudentId().substring(0, 1));
-        Long classNumber = Long.parseLong(who.getStudentId().substring(1, 2));
+        if(me.getStudentId().substring(0, 1).equals(user.getStudentId().substring(0,1))) return 0;
+        else {
+            Long grade = Long.parseLong(user.getStudentId().substring(0, 1));
+            Long classNumber = Long.parseLong(user.getStudentId().substring(1, 2));
 
-        SchoolClass schoolClass = schoolClassFacade.currentSchoolClass(grade, classNumber);
+            SchoolClass schoolClass = schoolClassFacade.currentSchoolClass(grade, classNumber);
 
-        schoolClass.addCandy100();
+            schoolClass.addCandy100();
+        }
 
         int socialScore, knowledgeScore, emotionScore, decisionScore;
 
-        socialScore = 100 - Math.abs(user.getSocialTypeScore() - who.getSocialTypeScore());
-        knowledgeScore = 100 - Math.abs(user.getKnowledgeTypeScore() - who.getKnowledgeTypeScore());
-        emotionScore = 100 - Math.abs(user.getEmotionTypeScore() - who.getEmotionTypeScore());
-        decisionScore = 100 - Math.abs(user.getDecisionTypeScore() - who.getDecisionTypeScore());
+        socialScore = 100 - Math.abs(me.getSocialTypeScore() - user.getSocialTypeScore());
+        knowledgeScore = 100 - Math.abs(me.getKnowledgeTypeScore() - user.getKnowledgeTypeScore());
+        emotionScore = 100 - Math.abs(me.getEmotionTypeScore() - user.getEmotionTypeScore());
+        decisionScore = 100 - Math.abs(me.getDecisionTypeScore() - user.getDecisionTypeScore());
 
         return (socialScore + knowledgeScore + emotionScore + decisionScore)/4;
     }
