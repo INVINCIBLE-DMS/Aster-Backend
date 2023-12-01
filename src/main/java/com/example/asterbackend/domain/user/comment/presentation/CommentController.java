5169da070d6742039a4e4101d13988fec155d5cb
dkflfkd53 <b57062562@gmail.com>
2023-12-01 16:27:45 +0900

    ♻️ :: 댓글 조회 api 수정

--- src/main/java/com/example/asterbackend/domain/user/comment/presentation/CommentController.java
+++ src/main/java/com/example/asterbackend/domain/user/comment/presentation/CommentController.java
@@ -1,16 +1,22 @@
 package com.example.asterbackend.domain.user.comment.presentation;
 
 import com.example.asterbackend.domain.user.comment.presentation.request.CommentRequest;
-import com.example.asterbackend.domain.user.comment.presentation.response.QueryCommentResponse;
+import com.example.asterbackend.domain.user.comment.presentation.response.FeedAndCommentsResponse;
 import com.example.asterbackend.domain.user.comment.service.comment.CreateCommentService;
 import com.example.asterbackend.domain.user.comment.service.comment.DeleteCommentService;
 import com.example.asterbackend.domain.user.comment.service.comment.QueryCommentService;
 import com.example.asterbackend.domain.user.comment.service.comment.UpdateCommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,7 +37,7 @@ public class CommentController {
     }
 
     @GetMapping("/{feedId}")
-    public List<QueryCommentResponse> queryComment(@PathVariable Long feedId) {
+    public FeedAndCommentsResponse queryComment(@PathVariable Long feedId) {
         return queryCommentService.queryComment(feedId);
     }
 
--- src/main/java/com/example/asterbackend/domain/user/comment/service/comment/QueryCommentService.java
+++ src/main/java/com/example/asterbackend/domain/user/comment/service/comment/QueryCommentService.java
@@ -1,7 +1,11 @@
 package com.example.asterbackend.domain.user.comment.service.comment;
 
+import com.example.asterbackend.domain.user.comment.presentation.response.FeedAndCommentsResponse;
 import com.example.asterbackend.domain.user.comment.presentation.response.QueryCommentResponse;
 import com.example.asterbackend.domain.user.comment.repository.CommentRepository;
+import com.example.asterbackend.domain.user.feed.entity.Feed;
+import com.example.asterbackend.domain.user.feed.facade.FeedFacade;
+import com.example.asterbackend.domain.user.feed.presentation.dto.response.FeedListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +16,21 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class QueryCommentService {
 
+    private final FeedFacade feedFacade;
+
     private final CommentRepository commentRepository;
 
-    public List<QueryCommentResponse> queryComment(Long feedId) {
-        return commentRepository.findAllByFeedId(feedId)
+    public FeedAndCommentsResponse queryComment(Long feedId) {
+        Feed feed = feedFacade.currentFeed(feedId);
+
+        FeedListResponse feedResponse = new FeedListResponse(feed);
+
+        List<QueryCommentResponse> commentList = commentRepository.findAllByFeedId(feedId)
                 .stream()
                 .map(QueryCommentResponse::new)
                 .collect(Collectors.toList());
+
+        return new FeedAndCommentsResponse(feedResponse, commentList);
     }
 
 }
