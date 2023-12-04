a52e3cb52a1f635cb2f6c084afc9189044533b31
tae.y <0211ilyoil@gmail.com>
2023-12-04 10:17:43 +0900

    테스트2

--- src/main/java/com/example/asterbackend/domain/user/auth/presentation/dto/response/MyInfoResponse.java
+++ src/main/java/com/example/asterbackend/domain/user/auth/presentation/dto/response/MyInfoResponse.java
@@ -2,7 +2,10 @@ package com.example.asterbackend.domain.user.auth.presentation.dto.response;
 
 import com.example.asterbackend.domain.user.user.entity.User;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @RequiredArgsConstructor
@@ -12,11 +15,15 @@ public class MyInfoResponse {
 
     private String studentId;
 
+    @NotNull
     private String profileImageUrl;
+
+    private String test;
 
     public MyInfoResponse(User user) {
         username = user.getUsername();
         studentId = user.getStudentId();
         profileImageUrl = user.getProfileImgUrl();
+        test = user.getUsername();
     }
 }
