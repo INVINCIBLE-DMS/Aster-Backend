42995cfe729918ceef7e591f320af8a1905d478c
tae.y <0211ilyoil@gmail.com>
2023-12-04 12:31:52 +0900

    ♻️:: 테스트 종료

--- src/main/java/com/example/asterbackend/domain/user/auth/presentation/dto/response/MyInfoResponse.java
+++ src/main/java/com/example/asterbackend/domain/user/auth/presentation/dto/response/MyInfoResponse.java
@@ -16,13 +16,10 @@ public class MyInfoResponse {
     private String studentId;
 
     private String profileImageUrl;
-
-    private String test;
-
+    
     public MyInfoResponse(User user) {
         username = user.getUsername();
         studentId = user.getStudentId();
         profileImageUrl = user.getProfileImgUrl();
-        test = user.getUsername();
     }
 }
